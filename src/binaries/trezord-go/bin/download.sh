5ad518a306911c6c1dcf86c063aa4e8b30d549c0
vdovhanych <dovhanych@me.com>
2021-10-27 15:30:34 +0200

    feat: adding necessary changes for arm64 bridge

--- src/binaries/trezord-go/bin/download.sh
+++ src/binaries/trezord-go/bin/download.sh
@@ -3,4 +3,5 @@ set -e
 
 # TODO: fetch trezord-go from nix
 
-nix-shell -p autoPatchelfHook --run "autoPatchelf trezord-*"
+nix-shell -p autoPatchelfHook --run "autoPatchelf ./src/binaries/trezord-go/bin/trezord-*"
+nix-shell -p autoPatchelfHook --run "patchelf --set-interpreter /nix/store/34k9b4lsmr7mcmykvbmwjazydwnfkckk-glibc-2.33-50/lib/ld-linux-aarch64.so.1 ./src/binaries/trezord-go/bin/*-arm64"
